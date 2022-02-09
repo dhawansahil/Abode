b526e2e54660e375d7da546af65f7e1b27f4775c
dhawansahil <70278793+dhawansahil@users.noreply.github.com>
2022-02-09 16:59:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+var express =  require("express");
+var bodyParser = require("body-parser");
+
+const app = express();
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname+ "/index.html");
+
+});
+
+app.get("/myWorks", function(req,res){
+    res.sendFile(__dirname+ "/Myworks.html");
+
+});
+
+app.get("/notification", function(req,res){
+    res.sendFile(__dirname+ "/notifications.html");
+
+});
+
+app.get("/profile", function(req,res){
+    res.sendFile(__dirname+ "/profile.html");
+
+});
+
+app.listen(3000,function(req,res){
+    console.log("Server Started at port 3000");
+});
